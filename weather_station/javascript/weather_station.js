848092fe065a009d833ba76a7107002643bb8338
Joakim Andre Tønnesen <joakim.tonnesen@nordicsemi.no>
2016-03-04 09:09:12 +0100

    .

--- weather_station/javascript/weather_station.js
+++ weather_station/javascript/weather_station.js
@@ -23,7 +23,7 @@ var weatherStationServiceUUID = '20080001-e36f-4648-91c6-9e86ead38764';
 //var pressureCharacteristicUUID = '20080003-e36f-4648-91c6-9e86ead38764';
 //var humidityCharacteristicUUID = '20080004-e36f-4648-91c6-9e86ead38764';
 //var configurationCharacteristicUUID = '20080005-e36f-4648-91c6-9e86ead38764';
-var demoCharacteristicUUID = '20080006-e36f-4648-91c6-9e86ead38764';
+var demoCharacteristicUUID = '20080002-e36f-4648-91c6-9e86ead38764';
 
 /*  User Interface Service
     ledCharacteristicUUID - write/read - 4 bytes - uint32_t - LED ID - Red - Green - Blue (LSB)
